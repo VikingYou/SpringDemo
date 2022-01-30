9e0ef99a8d6464e313d26b434a67bc607cfb4146
bafeimao <ycd200709>
2022-01-30 17:37:49 +0800

    uploading FilesDone

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,39 @@
 package com.example.demo;
 
+import com.example.demo.consumingrest.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
-@EnableScheduling
-@SpringBootApplication
+import java.io.IOException;
+
+//@EnableScheduling
+//@SpringBootApplication
 public class DemoApplication {
+    private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder.build();
+//    }
+
+//    @Bean
+//    public CommandLineRunner run(RestTemplate restTemplate) throws IOException {
+//        return args -> {
+//            Quote quote = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random",
+//                    Quote.class);
+//            log.info(quote.toString());
+//        };
+//    }
 
 }
